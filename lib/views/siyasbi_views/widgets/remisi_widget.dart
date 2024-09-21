1769cbb48a12a9901320c85044c16d5c5b5a0dbd
k47tan <hashby.alhadi@gmial.com>
2024-09-22 03:35:23 +0700

    refactor: Update imports and add SVG icons to RemisiWidget

--- lib/views/siyasbi_views/widgets/remisi_widget.dart
+++ lib/views/siyasbi_views/widgets/remisi_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../../constants/color_constant.dart';
 import '../../../constants/text_style_constant.dart';
 
@@ -39,8 +40,134 @@ class RemisiWidget extends StatelessWidget {
               ),
             ),
           ),
-          const SizedBox(height: 18),
-          const SizedBox(height: 18),
+          const SizedBox(height: 16),
+          Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 5,
+                    offset: Offset(3, 3),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset('assets/svg/sisa_pidana.svg',
+                      width: 50, height: 50),
+                  SizedBox(width: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sisa Pidana Setelah Potongan Remisi',
+                        style: TextStyleConstant.nunitoRegular.copyWith(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '3 Tahun 3 Bulan',
+                        style: TextStyleConstant.nunitoRegular.copyWith(
+                          fontSize: 12,
+                          color: ColorConstant.paragraphTextColor,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )),
+          const SizedBox(height: 8),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 5,
+                  offset: Offset(3, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                SvgPicture.asset('assets/svg/tanggal_bebas.svg',
+                    width: 50, height: 50),
+                SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Tanggal Bebas Menjalani Pidana',
+                      style: TextStyleConstant.nunitoRegular.copyWith(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      '12-12-2021',
+                      style: TextStyleConstant.nunitoRegular.copyWith(
+                        fontSize: 10,
+                        color: ColorConstant.paragraphTextColor,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(bottom: 5, left: 5),
+            decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: ColorConstant.primaryBackground)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    SvgPicture.asset('assets/svg/riwayat_remisi.svg',
+                        width: 20, height: 22),
+                    const SizedBox(width: 8),
+                    Text(
+                      'Riwayat Remisi',
+                      style: TextStyleConstant.nunitoRegular.copyWith(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Selengkapnya',
+                      style: TextStyleConstant.nunitoRegular.copyWith(
+                        fontSize: 12,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: ColorConstant.primaryColor,
+                      size: 8,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
