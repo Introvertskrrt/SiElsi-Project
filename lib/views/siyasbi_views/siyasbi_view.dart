5c14760aba1f015937d4cd00a96527741f59bb88
k47tan <hashby.alhadi@gmial.com>
2024-09-23 02:12:23 +0700

    refactor: Update SiyasbiView layout and imports

--- lib/views/siyasbi_views/siyasbi_view.dart
+++ lib/views/siyasbi_views/siyasbi_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sibesi_app/constants/color_constant.dart';
 import 'package:sibesi_app/constants/text_style_constant.dart';
 import 'package:sibesi_app/views/siyasbi_views/widgets/saldo_widget.dart';
@@ -11,50 +12,57 @@ class SiyasbiView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      // Memindahkan DefaultTabController ke sini
-      length: 4, // Jumlah tab
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'SiYASBI',
-            style: TextStyleConstant.nunitoBold.copyWith(
-              fontSize: 20,
-              color: Colors.white,
-            ),
+    return Scaffold(
+      backgroundColor: ColorConstant.primaryBackground,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.white,
+            size: 20,
           ),
-          centerTitle: true,
-          backgroundColor: ColorConstant.primaryColor,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        title: Text(
+          'SiYASBI',
+          style: TextStyleConstant.nunitoSemiBold.copyWith(
+            fontSize: 20,
+            color: Colors.white,
           ),
         ),
-        body: SingleChildScrollView(
-          child: const Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(height: 18),
-                    HeaderWidget(),
-                    SizedBox(height: 18),
-                    BarWidget(),
-                    SizedBox(height: 18),
-                    RemisiWidget(),
-                    SizedBox(height: 18),
-                    const SaldoWidget(),
-                    SizedBox(height: 30),
-                  ],
-                ),
-              ),
-            ],
+        centerTitle: true,
+        backgroundColor: ColorConstant.primaryColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
           ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: const Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 18),
+                  HeaderWidget(),
+                  SizedBox(height: 18),
+                  BarWidget(),
+                  SizedBox(height: 18),
+                  RemisiWidget(),
+                  SizedBox(height: 18),
+                  const SaldoWidget(),
+                  SizedBox(height: 30),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
